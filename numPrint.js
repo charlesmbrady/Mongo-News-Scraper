ce612085442d282b1be56a591bd80ec7fb46f608
Charles <charlesmbrady@gmail.com>
2019-04-23 20:42:11 -0400

    adding more files/folder

--- numPrint.js
+++
@@ -1,9 +0,0 @@
-function numPrint(x) {
-    if(typeof x == 'number'){
-        return x;
-    }
-    
-}
-console.log(numPrint(33));
-
-module.exports = numPrint;
--- test/numPrint.test.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect;
-const numPrint = require('../numPrint');
-
-describe('test for numPrint', function(){
-
-    it('Should return the number', function(){
-        expect(numPrint(4)).to.equal(4)
-    })
-})
